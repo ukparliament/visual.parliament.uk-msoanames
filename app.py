93e4fc857fd9cb1ea2f5a22a3386f5a4431c6da9
Oliver Hawkins <oli@olihawkins.com>
2018-08-02 17:17:05 +0100

    0.0.1 - Initial commit

---
+++ app.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# Prototype Flask app for MSOA names
+
+# Imports --------------------------------------------------------------------
+
+import os
+import psycopg2
+
+from flask import Flask
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+
+# Application ----------------------------------------------------------------
+
+app = Flask(__name__)
+
+# Constants ------------------------------------------------------------------
+
+MSOA_DB_HOST = os.environ['MSOA_DB_HOST']
+MSOA_DB_PORT = os.environ['MSOA_DB_PORT']
+MSOA_DB_NAME = os.environ['MSOA_DB_NAME']
+MSOA_DB_USERNAME = os.environ['MSOA_DB_USERNAME']
+MSOA_DB_PASSWORD = os.environ['MSOA_DB_PASSWORD']
+
+# Routes ---------------------------------------------------------------------
+
+@app.route('/')
+def map():
+    return render_template('map.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        con = connect()
+        cur = con.cursor()
+        cur.execute('''
+            INSERT INTO suggestions (
+                ip,
+                msoa11cd,
+                msoa11nm,
+                msoa11hclnm,
+                suggestion,
+                reason)
+            VALUES(%s, %s, %s, %s, %s, %s);''',
+                (request.remote_addr,
+                request.form['msoa11cd'],
+                request.form['msoa11nm'],
+                request.form['msoa11hclnm'],
+                request.form['suggestion'],
+                request.form['reason']))
+        con.commit()
+        cur.close()
+        con.close()
+        return '''{{
+            "msoa11cd": "{0}",
+            "msoa11cd": "{1}",
+            "msoa11nm": "{2}",
+            "msoa11hclnm": "{3}",
+            "suggestion": "{4}",
+            "reason": "{5}"}}'''.format(
+            request.remote_addr,
+            request.form['msoa11cd'],
+            request.form['msoa11nm'],
+            request.form['msoa11hclnm'],
+            request.form['suggestion'],
+            request.form['reason'])
+    else:
+        return redirect(url_for('map'))
+
+# Database -------------------------------------------------------------------
+
+def connect():
+    con = psycopg2.connect( \
+        database=MSOA_DB_NAME,
+        user=MSOA_DB_USERNAME,
+        password=MSOA_DB_PASSWORD,
+        host=MSOA_DB_HOST,
+        port=MSOA_DB_PORT,
+        connect_timeout=10)
+
+    return con
+
+# Main -----------------------------------------------------------------------
+
+if __name__ == "__main__":
+    # app.debug = True
+    app.run()
---
+++ src/map.js
@@ -0,0 +1,381 @@
+// Front-end component
+
+// Factories -----------------------------------------------------------------
+const createMap = () => {
+
+	mapboxgl.accessToken =
+		"pk.eyJ1IjoiaGF3a2luc29ob2NsIiwiYSI6IjNmMmZkMzY4MTUw" +
+		"ZGNiMjE4NTExOWQwNDBjNzg4NjAzIn0.bv8BBlUo7MtQR544YviGZQ";
+
+	const map = new mapboxgl.Map({
+		container: "map",
+		style: "mapbox://styles/hawkinsohocl/cjkcm2ii04c9u2sqgjhzr0hwt"
+	});
+
+	return map;
+};
+
+const createApp = (map) => ({
+
+	suggestion: null,
+	map: map,
+
+	run() {
+
+		// Set up the map
+		map.addControl(new mapboxgl.NavigationControl(), "top-right");
+		// map.addControl(new mapboxgl.GeolocateControl({
+		// 	showUserLocation: false,
+		// 	fitBoundsOptions: {maxZoom: 12.5}
+		// }));
+		map.getCanvas().style.cursor = "pointer";
+
+		this.map.on("load", () => {
+
+			map.addSource("msoa", {
+		        "type": "vector",
+		        "url": "mapbox://hawkinsohocl.dqzrnokh"
+		    });
+
+			map.addLayer({
+				"id": "msoa-highlight",
+				"type": "fill",
+				"source": "msoa",
+				"source-layer": "msoa-2011-polygons-hcl-0qsdgc",
+				"paint": {
+					"fill-color": "#d83808",
+					"fill-opacity": 0
+				},
+				"minzoom": 8,
+				"maxzoom": 22
+			});
+		});
+
+		map.on("click", createClickHandler(this));
+
+		map.on('zoom', () => {
+			if (map.getZoom() < 8 && this.hasSuggestion()) {
+				this.closeAndRemoveSuggestion();
+			}
+		});
+
+		return this;
+	},
+
+	getSuggestion(suggestion) {
+		return this.suggestion;
+	},
+
+	setSuggestion(suggestion) {
+
+		this.suggestion = suggestion;
+		this.suggestion.open();
+		return this;
+	},
+
+	removeSuggestion() {
+
+		this.suggestion.clearAnimation();
+		this.suggestion.deselect();
+		this.suggestion = null;
+		return this;
+	},
+
+	closeAndRemoveSuggestion() {
+
+		this.suggestion.close();
+		this.removeSuggestion();
+		return this;
+	},
+
+	hasSuggestion() {
+		return this.suggestion !== null;
+	}
+});
+
+const createSuggestion = (map, createForm, properties, coordinates) => ({
+
+	map: map,
+	createForm: createForm,
+	properties: properties,
+	coordinates: coordinates,
+	timestamp: Date.now(),
+	window: null,
+	animation: null,
+	animating: false,
+
+	getId() { return `${this.properties.msoa11cd}${this.timestamp}` },
+	getCode() { return this.properties.msoa11cd; },
+	getName() { return this.properties.msoa11hclnm; },
+
+	open() {
+
+		// Highlight the suggestion's feature
+		this.map.setPaintProperty(
+			"msoa-highlight",
+			"fill-opacity",
+			["case", ["==", ["get", "msoa11cd"],
+				this.properties.msoa11cd], 0.2, 0.0]);
+
+		// Open the suggestion window
+		this.window = new mapboxgl.Popup({closeButton: false})
+			.setLngLat(this.coordinates)
+			.setDOMContent(this.createForm(this.properties))
+			.addTo(this.map);
+
+		this.window.on("close", () => this.clearAnimation());
+	},
+
+	close() {
+		// Close the suggestion window if it exists
+		if (this.window !== null) this.window.remove();
+	},
+
+	deselect() {
+		// Remove the highlight from the suggestion's feature
+		this.map.setPaintProperty("msoa-highlight", "fill-opacity", 0.0);
+	},
+
+	getAnimation() {
+
+		const message = "Sending ";
+		let output = message;
+		let i = 0;
+
+		return () => {
+
+			if (i == 3) {
+				output = message;
+				i = 0;
+			} else {
+				output += ".";
+				i = i + 1;
+			}
+
+			document.getElementById("statusmsg").innerHTML = "";
+			document.getElementById("statusmsg").innerHTML = output;
+		}
+	},
+
+	startAnimation() {
+		if (! this.animating) {
+			this.animating = true;
+			this.animation = setInterval(this.getAnimation(), 330);
+		}
+	},
+
+	endAnimation(statusMsg) {
+		if (this.animating) {
+			clearInterval(this.animation);
+			this.animating = false;
+			document.getElementById("statusmsg").innerHTML = statusMsg;
+		}
+	},
+
+	clearAnimation() {
+		if (this.animating) {
+			clearInterval(this.animation);
+			this.animating = false;
+		}
+	}
+});
+
+const createClickHandler = (app) => {
+
+	const createForm = createFormFactory(app);
+
+	return (e) => {
+
+		// Get the feature under the pointer
+		const feature = app.map.queryRenderedFeatures(
+			e.point, {layers: ["msoa-highlight"]})[0];
+
+		// If there is no feature under the pointer just remove the suggestion
+		if (feature === undefined) {
+			if (app.hasSuggestion()) {
+				app.removeSuggestion();
+			}
+			return;
+		}
+
+		// Get the area code for the feature
+		const clickedCode = feature.properties.msoa11cd;
+
+		// If the click is on an open suggestion, remove it
+		if (app.hasSuggestion() &&
+			app.suggestion.getCode() == clickedCode) {
+			app.removeSuggestion();
+			return;
+		}
+
+		// Otherwise create a new suggestion
+		app.setSuggestion(createSuggestion(
+			app.map, createForm, feature.properties, e.lngLat));
+	};
+};
+
+const createFormFactory = (app) => {
+
+	const suggestionHandler = (e) => {
+
+		const sendButton = document.getElementById("sendbutton");
+
+		if (e.srcElement.value == 0) {
+
+			sendButton.disabled = true;
+			sendButton.style.background = "#c0c0c0";
+			sendButton.style.borderColor = "#c0c0c0";
+
+		} else{
+
+			sendButton.disabled = false;
+			sendButton.style.background = "#d06040";
+			sendButton.style.borderColor = "#d06040";
+		}
+	};
+
+	const sendHandler = () => {
+
+		const sid = app.getSuggestion().getId();
+		const msoa11cd = document.getElementById("msoa11cd").value;
+		const msoa11nm = document.getElementById("msoa11nm").value;
+		const msoa11hclnm = document.getElementById("msoa11hclnm").value;
+		const suggestion = document.getElementById("suggestion").value;
+		const reason = document.getElementById("reason").value;
+
+		// Check input lengths are sane
+		if (msoa11cd.length == 0 || msoa11nm.length == 0 ||
+			msoa11hclnm.length == 0 || suggestion.length == 0 ) {
+			return;
+		}
+
+		if (msoa11cd.length > 64 || msoa11nm.length > 64 ||
+			msoa11hclnm.length > 64 || suggestion.length > 64 ||
+			reason.length > 1024) {
+			return;
+		}
+
+		// Change the form content to the status box
+		const contentBox = document.getElementById('contentbox');
+		contentBox.innerHTML = "";
+		contentBox.appendChild(createStatus());
+		app.getSuggestion().startAnimation();
+
+		// Send the request
+		const formData = new FormData();
+		formData.append("msoa11cd", msoa11cd);
+		formData.append("msoa11nm", msoa11nm);
+		formData.append("msoa11hclnm", msoa11hclnm);
+		formData.append("suggestion", suggestion);
+		formData.append("reason", reason);
+
+		const request = new XMLHttpRequest();
+		request.timeout = 20000;
+
+		request.onload = () => {
+
+			if (app.hasSuggestion() && sid == app.getSuggestion().getId()) {
+				app.getSuggestion().endAnimation("All done!");
+			}
+		};
+
+		request.ontimeout = () => {
+
+			if (app.hasSuggestion() && sid == app.getSuggestion().getId()) {
+				app.getSuggestion().endAnimation("Could not reach the server.");
+			}
+		};
+
+		request.onerror = () => {
+
+			if (app.hasSuggestion() && sid == app.getSuggestion().getId()) {
+				app.getSuggestion().endAnimation("Could not reach the server.");
+			}
+		};
+
+		request.open("POST", "/submit");
+		request.send(formData);
+	};
+
+	const closeHandler = () => {
+		app.closeAndRemoveSuggestion();
+	};
+
+	const createStatus = (suggestion) => {
+
+		const html = `
+			<p>Thank you very much for your suggestion for this area.</p>
+			<p id="statusmsg">Sending </p>
+			<button class="closebutton" type="button">Close</button>`;
+
+		const status = document.createElement("div");
+		status.className = "statusbox";
+		status.innerHTML = html;
+
+		const closeButton = status.getElementsByClassName("closebutton")[0];
+		closeButton.addEventListener("click", closeHandler, false);
+
+		return status;
+	}
+
+	return (properties) => {
+
+		const suggestion = "Your suggestion (required)";
+		const reason = "Why you think this would be a better name";
+		const message = "Please note that we cannot accept suggestions for " +
+			"boundary changes.";
+
+		const html = `
+			<form method="post" action="/submit">
+				<input type="hidden" id="msoa11cd"
+					value="${properties.msoa11cd}">
+				<input type="hidden" id="msoa11nm"
+						value="${properties.msoa11nm}">
+				<input type="hidden" id="msoa11hclnm"
+						value="${properties.msoa11hclnm}">
+				<div id="namebox">
+					<p>${properties.msoa11hclnm}</p>
+				</div>
+				<div id="contentbox">
+				    <div>
+				        <input type="text" id="suggestion" class="suggestion"
+							placeholder="${suggestion}" maxlength="64" required>
+				    </div>
+				    <div>
+				        <textarea id="reason" placeholder="${reason}"
+							maxlength="1024"></textarea>
+				    </div>
+				    <div class="buttonbox">
+						<button id="sendbutton" class="sendbutton"
+							type="button" disabled>Submit</button>
+				        <button class="closebutton"
+							type="button">Close</button>
+				    </div>
+					<div id="messagebox">
+						<p>${message}</p>
+					</div>
+				</div>
+			</form>`;
+
+		const form = document.createElement("div");
+		form.className = "formbox";
+		form.innerHTML = html;
+
+		const suggestionInput = form.getElementsByClassName("suggestion")[0];
+		suggestionInput.addEventListener("input", suggestionHandler, false);
+
+		const sendButton = form.getElementsByClassName("sendbutton")[0];
+		sendButton.addEventListener("click", sendHandler, false);
+
+		const closeButton = form.getElementsByClassName("closebutton")[0];
+		closeButton.addEventListener("click", closeHandler, false);
+
+		return form;
+	};
+};
+
+// Main ----------------------------------------------------------------------
+
+const map = createMap();
+const app = createApp(map);
+app.run();
