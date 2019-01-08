df0460bf01b30bd0230e955225470877705ee51b
Oliver Hawkins <oli@olihawkins.com>
2019-01-08 17:43:36 +0000

    Updated placenames with latest suggestions

--- src/map.js
+++ src/map.js
@@ -23,7 +23,7 @@ const createMap = (long, lat, zoom) => {
 
     const map = new mapboxgl.Map({
         container: "map",
-        style: "mapbox://styles/hawkinsohocl/cjpo9mons0ffa2rp99z3op5bw",
+        style: "mapbox://styles/hawkinsohocl/cjqo18am107vt2rqi4ahmnv42",
         center: [longitude, latitude],
         zoom: zoomLevel
     });
@@ -51,14 +51,14 @@ const createApp = (map, csrf) => ({
 
             map.addSource("msoa", {
                 "type": "vector",
-                "url": "mapbox://hawkinsohocl.07ddkl5y"
+                "url": "mapbox://hawkinsohocl.8tfcupak"
             });
 
             map.addLayer({
                 "id": "msoa-highlight",
                 "type": "fill",
                 "source": "msoa",
-                "source-layer": "msoa-2011-polygons-hcl-7azd96",
+                "source-layer": "msoa-2011-polygons-hcl-63m4lt",
                 "paint": {
                     "fill-color": "#d83808",
                     "fill-opacity": 0
