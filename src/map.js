d567e6b98fb964796afaac9e84642a0ac0f47024
Oliver Hawkins <oli@olihawkins.com>
2019-01-21 13:33:11 +0000

    Updated placenames with new suggestions

--- src/map.js
+++ src/map.js
@@ -23,7 +23,7 @@ const createMap = (long, lat, zoom) => {
 
     const map = new mapboxgl.Map({
         container: "map",
-        style: "mapbox://styles/hawkinsohocl/cjqo18am107vt2rqi4ahmnv42",
+        style: "mapbox://styles/hawkinsohocl/cjr6cgqh30y452srwno8wdsxx",
         center: [longitude, latitude],
         zoom: zoomLevel
     });
@@ -51,14 +51,14 @@ const createApp = (map, csrf) => ({
 
             map.addSource("msoa", {
                 "type": "vector",
-                "url": "mapbox://hawkinsohocl.8tfcupak"
+                "url": "mapbox://hawkinsohocl.5yln3fsd"
             });
 
             map.addLayer({
                 "id": "msoa-highlight",
                 "type": "fill",
                 "source": "msoa",
-                "source-layer": "msoa-2011-polygons-hcl-63m4lt",
+                "source-layer": "msoa-2011-polygons-hcl-60j92x",
                 "paint": {
                     "fill-color": "#d83808",
                     "fill-opacity": 0
