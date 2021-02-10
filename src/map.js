25729e7bbcfa793e8f4f7b40498489e36a229c58
Oliver Hawkins <oli@olihawkins.com>
2021-02-10 12:34:00 +0000

    Updated MSOA names to version 1.12

--- src/map.js
+++ src/map.js
@@ -22,7 +22,7 @@ const createMap = (long, lat, zoom) => {
 
     const map = new mapboxgl.Map({
         container: "map",
-        style: "mapbox://styles/hawkinsohocl/ckkfffio500ob17lss9g91ysh", 
+        style: "mapbox://styles/hawkinsohocl/ckkzefcl47u5s17o5f29ylgt3", 
         center: [longitude, latitude],
         zoom: zoomLevel
     });
@@ -52,14 +52,14 @@ const createApp = (map, csrf) => ({
 
             map.addSource("msoa", {
                 "type": "vector",
-                "url": "mapbox://hawkinsohocl.8ljj16x3" 
+                "url": "mapbox://hawkinsohocl.dhbsy81x" 
             });
 
             map.addLayer({
                 "id": "msoa-highlight",
                 "type": "fill",
                 "source": "msoa",
-                "source-layer": "msoa-2011-polygons-hcl-dv20wz",
+                "source-layer": "msoa-2011-polygons-hcl-5s1jn6",
                 "paint": {
                     "fill-color": "#682f7f",
                     "fill-opacity": 0
@@ -214,8 +214,8 @@ const createMessageFactory = (app) => {
                 <p>${properties.msoa11nm}</p>
                 <p>${properties.msoa11cd}</p>
                 <p>
-                    <a href="/msoanames/static/MSOA-Names-1.11.xlsx">Excel</a> /
-                    <a href="/msoanames/static/MSOA-Names-1.11.csv">CSV</a>
+                    <a href="/msoanames/static/MSOA-Names-1.12.xlsx">Excel</a> /
+                    <a href="/msoanames/static/MSOA-Names-1.12.csv">CSV</a>
                 </p>
             </div>
             <div id="buttonbox">
